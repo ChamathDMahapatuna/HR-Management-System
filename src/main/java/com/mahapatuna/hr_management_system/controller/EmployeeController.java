f686fbd4692f37405ce5a72e5f099d05598de82c
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2025-07-03 23:57:33 +0530

    Create Folder structure and Dependancies

--- src/main/java/com/mahapatuna/hr_management_system/controller/EmployeeController.java
+++ src/main/java/com/mahapatuna/hr_management_system/controller/EmployeeController.java
@@ -1,4 +1,4 @@
-package com.mahapatuna.hr_management_system;
+package com.mahapatuna.hr_management_system.controller;
 
 @org.springframework.stereotype.Controller
 public class EmployeeController {
